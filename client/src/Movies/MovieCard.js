3a0213bb9bda0169dfb9fec322f3e00a1028a1d4
Matt Conway <mattconway727@gmail.com>
2020-05-06 09:54:50 -0400

    MVP and Stretch - Edited and Complete

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const { title, director, metascore, stars } = props.movie;
-
 const MovieCard = (props) => {
+	const { title, director, metascore, stars } = props.movie;
+
 	return (
 		<div className="movie-card">
 			<h2>{title}</h2>
