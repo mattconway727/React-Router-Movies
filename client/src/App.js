24c583a86cc2c0206f91629b8a3d478fe04bb265
Matt Conway <mattconway727@gmail.com>
2020-05-06 09:51:54 -0400

    MVP and Stretch complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,30 @@
 import React, { useState } from 'react';
-
+import { Route, Switch } from 'react-router-dom';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
 import SavedList from './Movies/SavedList';
 
 const App = () => {
-  const [savedList, setSavedList] = useState( [] );
+	const [ savedList, setSavedList ] = useState([]);
 
-  const addToSavedList = movie => {
-    setSavedList( [...savedList, movie] );
-  };
+	const addToSavedList = (movie) => {
+		setSavedList([ ...savedList, movie ]);
+	};
 
-  return (
-    <div>
-      <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
-    </div>
-  );
+	return (
+		<div>
+			<SavedList list={savedList} />
+			<div>
+				<Switch>
+					<Route path="/movies/:id">
+						<Movie items={savedList} />
+					</Route>
+					<Route path="/">
+						<MovieList items={savedList} />
+					</Route>
+				</Switch>
+			</div>
+		</div>
+	);
 };
-
 export default App;
